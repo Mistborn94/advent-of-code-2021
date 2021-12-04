fa708b571975ef039b32ff3d0709a979c683ee43
Renette Ros <supergirl0894+git@gmail.com>
2021-12-04 09:31:02 +0200

    Solve day 4 using recursion

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -4,33 +4,32 @@ fun solveA(lines: List<String>): Int {
     val calledNumbers = lines.first().split(",").map { it.toInt() }.iterator()
     val boards = buildBoards(lines.subList(2, lines.size))
 
-    var call = 0
-    while (!boards.any { it.hasWon() }) {
-        call = calledNumbers.next()
-        boards.forEach { board -> board.mark(call) }
-    }
+    return getWinningBoard(boards, calledNumbers).score()
+}
 
-    return boards.first { it.hasWon() }.score() * call
+private tailrec fun getWinningBoard(boards: List<Board>, calledNumbers: Iterator<Int>): Board {
+    return if (boards.any { it.hasWon() }) {
+        boards.first { it.hasWon() }
+    } else {
+        val calledNumber = calledNumbers.next()
+        getWinningBoard(boards.onEach { it.mark(calledNumber) }, calledNumbers)
+    }
 }
 
 fun solveB(lines: List<String>): Int {
     val calledNumbers = lines.first().split(",").map { it.toInt() }.iterator()
     val boards = buildBoards(lines.subList(2, lines.size)).toMutableList()
 
-    var call = 0
-    while (boards.size > 1) {
-        call = calledNumbers.next()
-        boards.forEach { board -> board.mark(call) }
-        boards.removeIf { it.hasWon() }
-    }
+    return getLastBoard(boards, calledNumbers).score()
+}
 
-    val lastBoard = boards.first()
-    while (!lastBoard.hasWon()) {
-        call = calledNumbers.next();
-        lastBoard.mark(call)
+private tailrec fun getLastBoard(boards: List<Board>, calledNumbers: Iterator<Int>): Board {
+    return if (boards.size == 1 && boards.first().hasWon()) {
+        boards.first()
+    } else {
+        val calledNumber = calledNumbers.next()
+        getLastBoard(boards.filter { !it.hasWon() }.onEach { it.mark(calledNumber) }, calledNumbers)
     }
-
-    return lastBoard.score() * call
 }
 
 private fun buildBoards(lines: List<String>) = lines
@@ -39,13 +38,15 @@ private fun buildBoards(lines: List<String>) = lines
     .map(::buildBoard)
 
 fun buildBoard(lists: List<String>): Board {
-    return lists.flatMapIndexed { y, row ->
-        row.trim().split("  ", " ").mapIndexed { x, value -> Pair(value.toInt(), BingoCell(x, y)) }
-    }.toMap().let(::Board)
+    return lists.flatMapIndexed { y, row -> parseRow(row, y) }
+        .toMap()
+        .let(::Board)
 }
 
-class Board(private val cells: Map<Int, BingoCell>) {
+private fun parseRow(row: String, y: Int) = row.chunked(3).mapIndexed { x, value -> Pair(value.trim().toInt(), BingoCell(x, y)) }
 
+class Board(private val cells: Map<Int, BingoCell>) {
+    private var lastMarked: Int = 0
     private val rowMarkCount = mutableMapOf<Int, Int>()
     private val columnMarkCount = mutableMapOf<Int, Int>()
 
@@ -54,13 +55,14 @@ class Board(private val cells: Map<Int, BingoCell>) {
     fun mark(number: Int) {
         cells[number]?.also {
             it.marked = true
+            lastMarked = number
             rowMarkCount[it.y] = rowMarkCount.getOrDefault(it.y, 0) + 1
             columnMarkCount[it.x] = columnMarkCount.getOrDefault(it.x, 0) + 1
         }
     }
 
     fun score(): Int = cells.entries.filter { (_, value) -> !value.marked }
-        .sumOf { (key, _) -> key }
+        .sumOf { (key, _) -> key } * lastMarked
 }
 
-class BingoCell(val x: Int, val y: Int, var marked: Boolean = false)
+data class BingoCell(val x: Int, val y: Int, var marked: Boolean = false)
