5037e19356f357c24f2a52e03538f0da1007b1a8
Renette Ros <supergirl0894+git@gmail.com>
2021-12-23 17:14:15 +0200

    Revert "Solve day 22 using fancy set theory things"
    This reverts commit 231b7649dcd6db6dc0a958eb5d0bb586ebabaff0. The original implementation is much faster for some reason

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -19,23 +19,31 @@ fun solveB(lines: List<String>): Long {
 }
 
 private fun doReboot(steps: List<RebootStep>): Long {
-    val (turnedOn, turnedOff) = steps
-        .fold(Pair(emptyList<Cuboid>(), emptyList<Cuboid>())) { (turnedOn, turnedOff), instruction ->
+    val turnedOn: List<Cuboid> = steps
+        .fold(emptyList()) { acc, instruction ->
             if (instruction.on) {
-                val newOn = turnedOn + instruction.cuboid + turnedOff.overlapAll(instruction.cuboid)
-                val newOff = turnedOff + turnedOn.overlapAll(instruction.cuboid)
-                newOn to newOff
+                acc + instruction.cuboid
             } else {
-                val newOn = turnedOn + turnedOff.overlapAll(instruction.cuboid)
-                val newOff = turnedOff + turnedOn.overlapAll(instruction.cuboid)
-                newOn to newOff
+                acc.flatMap { it.subtract(instruction.cuboid) }
             }
         }
 
-    return turnedOn.sumOf { it.volume } - turnedOff.sumOf { it.volume }
+    return deduplicate(turnedOn).sumOf { it.count }
 }
 
-private fun List<Cuboid>.overlapAll(cuboid: Cuboid) = map { it.overlap(cuboid) }.filter { !it.isEmpty() }
+fun deduplicate(cuboids: Collection<Cuboid>): Collection<Cuboid> {
+    val counted = mutableListOf<Cuboid>()
+
+    for (next in cuboids) {
+        val overlaps = counted.asSequence().filter { it.overlaps(next) }
+        val new = overlaps.fold(sequenceOf(next)) { new, existing ->
+            new.flatMap { it.subtract(existing) }
+        }
+        counted.addAll(new)
+    }
+
+    return counted
+}
 
 fun parseLine(line: String): RebootStep {
     val matchResult = pattern.matchEntire(line)!!
@@ -52,11 +60,40 @@ data class RebootStep(val on: Boolean, val cuboid: Cuboid) {
 
 data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange) {
 
-    val volume: Long = x.size().toLong() * y.size().toLong() * z.size().toLong()
+    val count: Long = x.size().toLong() * y.size().toLong() * z.size().toLong()
 
     fun isEmpty() = x.isEmpty() || y.isEmpty() || z.isEmpty()
 
+    fun overlaps(other: Cuboid): Boolean = overlap(other).count > 0
     fun overlap(other: Cuboid): Cuboid = Cuboid(overlappingRange(x, other.x), overlappingRange(y, other.y), overlappingRange(z, other.z))
+
+    fun subtract(other: Cuboid): List<Cuboid> {
+        val overlap = overlap(other)
+        return when {
+            overlap.isEmpty() -> listOf(this)
+            overlap == this -> emptyList()
+            else -> {
+                val possibleX = splitRange(x, overlap.x)
+                val possibleY = splitRange(y, overlap.y)
+                val possibleZ = splitRange(z, overlap.z)
+
+                listOf(
+                    Cuboid(x, possibleY.first(), z),
+                    Cuboid(x, possibleY.last(), z),
+                    Cuboid(possibleX.first(), possibleY[1], z),
+                    Cuboid(possibleX.last(), possibleY[1], z),
+                    Cuboid(possibleX[1], possibleY[1], possibleZ.first()),
+                    Cuboid(possibleX[1], possibleY[1], possibleZ.last()),
+                ).filter { !it.isEmpty() }
+            }
+        }
+    }
+
+    private fun splitRange(range: IntRange, overlapRange: IntRange): List<IntRange> {
+        val before = range.first until overlapRange.first
+        val after = overlapRange.last + 1..range.last
+        return listOf(before, overlapRange, after)
+    }
 
     private fun overlappingRange(first: IntRange, second: IntRange) = max(first.first, second.first)..min(first.last, second.last)
 }
